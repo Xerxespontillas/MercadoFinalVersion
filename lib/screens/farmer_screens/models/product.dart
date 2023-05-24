40c7ef3760a2fd2304fe60848fc65c79e7095b38
MisterRenz <johnrenz.valencia@cit.edu>
2023-05-25 03:39:49 +0800

    Added My Product Page and Drawer

---
+++ lib/screens/farmer_screens/models/product.dart
@@ -0,0 +1,19 @@
+class Product {
+  final String id;
+  final String productName;
+  final String productDetails;
+  final String image;
+  final double price;
+  final int quantity;
+  final String sellerName;
+
+  Product({
+    required this.id,
+    required this.productName,
+    required this.productDetails,
+    required this.image,
+    required this.price,
+    required this.quantity,
+    required this.sellerName,
+  });
+}
